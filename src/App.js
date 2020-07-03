05c19e0f1c23135e0e557778ff55f8dacaf8c394
Nishant garg <iamnishantgarg@gmail.com>
2020-07-03 10:18:59 +0530

    search box woking

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { useState, useEffect } from "react";
 import CharacterGrid from "./components/Characters/CharacterGrid";
 import axios from "axios";
+import Search from "./components/ui/Search";
 import "./App.css";
 import Header from "./components/ui/Header";
 const App = () => {
   const [items, setItems] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [query, setQuery] = useState("");
   useEffect(() => {
     const fetchItems = async () => {
       const result = await axios(
-        `https://www.breakingbadapi.com/api/characters`
+        `https://www.breakingbadapi.com/api/characters/?name=${query}`
       );
       // console.log(result.data);
       setItems(result.data);
       setIsLoading(false);
     };
     fetchItems();
-  }, []);
+  }, [query]);
 
   return (
     <div className="container">
       <Header />
+      <Search setQuery={(v) => setQuery(v)} />
       <CharacterGrid isLoading={isLoading} items={items} />
     </div>
   );
