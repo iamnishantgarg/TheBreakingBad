a9e4911ffd27b13332ba3b8c73a809cffa485927
Nishant garg <iamnishantgarg@gmail.com>
2020-07-03 10:05:13 +0530

    card grid displayed

--- src/components/Characters/CharacterGrid.js
+++ src/components/Characters/CharacterGrid.js
@@ -1,7 +1,15 @@
 import React from "react";
-
+import CharacterItem from "./CharacterItem";
 const CharacterGrid = ({ isLoading, items }) => {
-  return isLoading ? <h1>Loading...</h1> : <section></section>;
+  return isLoading ? (
+    <h1>Loading...</h1>
+  ) : (
+    <section className="cards">
+      {items.map((item, ind) => (
+        <CharacterItem item={item} key={ind} />
+      ))}
+    </section>
+  );
 };
 
 export default CharacterGrid;
