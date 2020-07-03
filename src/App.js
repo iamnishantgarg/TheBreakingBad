225d88d075b44211740c08aaf5b1195fa9bd035f
Nishant garg <iamnishantgarg@gmail.com>
2020-07-03 09:29:27 +0530

    header done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 // import logo from './logo.svg';
 import "./App.css";
-
+import Header from "./components/ui/Header";
 function App() {
-  return <div className="container">hy</div>;
+  return (
+    <div className="container">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
